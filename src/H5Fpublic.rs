755df54d91a1d477884eebef985e8a5e45f5eb48
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-26 16:26:33 -0400

    Fix styles

--- src/H5Fpublic.rs
+++ src/H5Fpublic.rs
@@ -4,11 +4,11 @@ use H5Ipublic::hid_t;
 use H5public::herr_t;
 
 pub const H5F_ACC_RDONLY: c_uint = 0x0000;
-pub const H5F_ACC_RDWR:   c_uint = 0x0001;
-pub const H5F_ACC_TRUNC:  c_uint = 0x0002;
-pub const H5F_ACC_EXCL:   c_uint = 0x0004;
-pub const H5F_ACC_DEBUG:  c_uint = 0x0008;
-pub const H5F_ACC_CREAT:  c_uint = 0x0010;
+pub const H5F_ACC_RDWR: c_uint = 0x0001;
+pub const H5F_ACC_TRUNC: c_uint = 0x0002;
+pub const H5F_ACC_EXCL: c_uint = 0x0004;
+pub const H5F_ACC_DEBUG: c_uint = 0x0008;
+pub const H5F_ACC_CREAT: c_uint = 0x0010;
 
 extern "C" {
     pub fn H5Fcreate(filename: *const c_char, flags: c_uint, create_plist: hid_t,
--- src/H5Tpublic.rs
+++ src/H5Tpublic.rs
@@ -6,18 +6,18 @@ use H5public::{herr_t, hsize_t};
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
 pub enum H5T_class_t {
-    H5T_NO_CLASS  = -1,
-    H5T_INTEGER   =  0,
-    H5T_FLOAT     =  1,
-    H5T_TIME      =  2,
-    H5T_STRING    =  3,
-    H5T_BITFIELD  =  4,
-    H5T_OPAQUE    =  5,
-    H5T_COMPOUND  =  6,
-    H5T_REFERENCE =  7,
-    H5T_ENUM      =  8,
-    H5T_VLEN      =  9,
-    H5T_ARRAY     = 10,
+    H5T_NO_CLASS = -1,
+    H5T_INTEGER = 0,
+    H5T_FLOAT = 1,
+    H5T_TIME = 2,
+    H5T_STRING = 3,
+    H5T_BITFIELD = 4,
+    H5T_OPAQUE = 5,
+    H5T_COMPOUND = 6,
+    H5T_REFERENCE = 7,
+    H5T_ENUM = 8,
+    H5T_VLEN = 9,
+    H5T_ARRAY = 10,
 
     H5T_NCLASSES,
 }
